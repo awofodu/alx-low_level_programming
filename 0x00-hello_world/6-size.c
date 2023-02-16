c96b5d62ac2e373d5f586a2602fa5754f14acd9d
Samuel Awofodu <awofodus@gmail.com>
2023-02-16 13:05:00 -0800

    refactor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Size of int: %c byte(s)\n", sizeof(int));
-	printf("Size of char: %c byte(s)\n", sizeof(char));
-	printf("Size of long int: %c byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %c byte(s)\n", sizeof(long long int));
-	printf("Size of float: %c byte(s)\n", sizeof(float));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
