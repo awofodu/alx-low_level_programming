8e71503f14881edf865b5bd426ad2d458818a711
Samuel Awofodu <awofodus@gmail.com>
2023-02-17 11:41:41 -0800

    New project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - function to print both lowercase and uppercase
+  * Return: 0
+  */
+
+int main(void)
+{
+	char sl = 'a';
+	char cl = 'A';
+	do
+	{
+		putchar(sl);
+		sl++;
+	}
+	while(sl <= 'z');
+
+	do
+	{
+		putchar(cl);
+		cl++;
+	}
+	while(cl <= 'Z');
+	putchar('\n');
+}
+
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * a program that prints the alphabet in lowercase, followed by a 
+  * Return: 0
+  */
+
+int main(void)
+{
+	char l = 'a'; 
+	for(char l = 'a'; l <= 'z'; l++)
+	{
+		if(l == 'e' || l == 'q')
+		{
+			continue;
+		}
+		putchar(l);
+	}
+	putchar('\n');
+}
+
