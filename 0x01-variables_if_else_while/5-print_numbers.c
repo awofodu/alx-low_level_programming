b16af328430e0c0fa88e27021e3adfa4e86985ed
Samuel Awofodu <awofodus@gmail.com>
2023-02-17 11:48:42 -0800

    Print base10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * a program that prints the alphabet in lowercase, followed by a 
+  * Return: 0
+  */
+
+int main(void)
+{
+
+	for(int n = 0;  n< 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
+
