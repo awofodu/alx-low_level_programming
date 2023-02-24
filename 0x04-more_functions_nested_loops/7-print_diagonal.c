ac700f531ff885d036f88779fe5cbccf6403ffa0
Samuel Awofodu <awofodus@gmail.com>
2023-02-24 02:03:47 -0800

    corrections on 7th project

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,10 +19,10 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; ++postn)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 1; space <= postn; ++space)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
