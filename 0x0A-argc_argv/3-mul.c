b4554728a02e3ce12deec51d4a729eb1036f5b37
Samuel Awofodu <awofodus@gmail.com>
2023-03-16 22:30:49 +0000

    releasing my own code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -56,18 +56,18 @@ int _atoi(char *s)
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
-  
+
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-  
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-  
+ 
 	printf("%d\n", result);
-  
+
 	return (0);
 }
